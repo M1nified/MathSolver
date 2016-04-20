e493d1ea26e9cdd38185e7c271c9a6d14d5516be
Minified <gora.pub@gmail.com>
2016-04-20 10:27:50 +0200

    AddEquationTest, LUP_SolveTest, LinearSolverTest1,...

--- UnitTests/LinearSolverTests.cs
+++ UnitTests/LinearSolverTests.cs
@@ -8,65 +8,76 @@ using System.Threading.Tasks;
 
 namespace Solver.Tests
 {
-	[TestClass()]
-	public class LinearSolverTests
-	{
-		[TestMethod()]
-		public void LinearSolverTest()
-		{
-			Solver.LinearSolver ls = new Solver.LinearSolver();
-		}
+				[TestClass()]
+				public class LinearSolverTests
+				{
+								Equation eq1 = new Equation(-2, 3, 1, 1);
+								Equation eq2 = new Equation(8, 2, 2, 3);
+								Equation eq3 = new Equation(6, 1, 3, 2);
+								int expected_equations_len = 3;
+								float[,] expected_matrixOfA = new float[3, 3] { { 3, 1, 1 }, { 2, 2, 3 }, { 1, 3, 2 } };
+								float[] expected_matrixOfB = new float[3] { -2, 8, 6 };
 
-		[TestMethod()]
-		public void LinearSolverTest1()
-		{
-			Equation eq1 = new Equation(-2, 3, 1, 1);
-			Equation eq2 = new Equation(8, 2, 2, 3);
-			Equation eq3 = new Equation(6, 1, 3, 2);
-			LinearSolver ls = new LinearSolver(eq1,eq2,eq3);
-			Assert.Fail();
-		}
+								float[] expected_solutionX = new float[3] { -2, 0, 4 };
+								LinearSolver ls;
 
-		[TestMethod()]
-		public void AddEquationTest()
-		{
-			Assert.Fail();
-		}
+								[TestInitialize]
+								public void TestInitialize()
+								{
+												ls = new LinearSolver(eq1, eq2, eq3);
+								}
+								[TestMethod()]
+								public void LinearSolverTest()
+								{
+												LinearSolver ls = new LinearSolver();
+								}
 
-		[TestMethod()]
-		public void UpdateMatrixTest()
-		{
-			Assert.Fail();
-		}
+								[TestMethod()]
+								public void LinearSolverTest1()
+								{
+												Assert.AreEqual(expected_equations_len, ls.equations.Count);
+												CollectionAssert.AreEqual(expected_matrixOfA, ls.matrixOfA);
+												CollectionAssert.AreEqual(expected_matrixOfB, ls.matrixOfB);
+								}
 
-		[TestMethod()]
-		public void ResolveTest()
-		{
-			Assert.Fail();
-		}
+								[TestMethod()]
+								public void AddEquationTest()
+								{
+												ls.AddEquation(eq1);
+												Assert.AreEqual(4, ls.equations.Count);
+								}
 
-		[TestMethod()]
-		public void ToStringTest()
-		{
-			Assert.Fail();
-		}
+								//[TestMethod()]
+								//public void UpdateMatrixTest()
+								//{
+								//				Assert.Fail();
+								//}
 
-		[TestMethod()]
-		public void LUP_Decomposition_1Test()
-		{
-			Assert.Fail();
-		}
+								//[TestMethod()]
+								//public void ResolveTest()
+								//{
+								//}
 
-		[TestMethod()]
-		public void LUP_DecompositionTest()
-		{
-			Assert.Fail();
-		}
+								//[TestMethod()]
+								//public void ToStringTest()
+								//{
+								//}
 
-		[TestMethod()]
-		public void LUP_SolveTest()
-		{
-			Assert.Fail();
-		}
-	}
+								//[TestMethod()]
+								//public void LUP_Decomposition_1Test()
+								//{
+								//}
+
+								//[TestMethod()]
+								//public void LUP_DecompositionTest()
+								//{
+								//}
+
+								[TestMethod()]
+								public void LUP_SolveTest()
+								{
+												ls.LUP_Solve();
+												CollectionAssert.AreEqual(expected_solutionX, ls.x);
+								}
+				}
 }
